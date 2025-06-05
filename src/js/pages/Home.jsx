fd6dc8c55a294b3809779dd272ec5b5753876dd5
WyattChase <esnaruto@yahoo.com>
2025-06-05 22:36:05 +0000

    fixed minor display issue

--- src/js/pages/Home.jsx
+++ src/js/pages/Home.jsx
@@ -6,7 +6,7 @@ import CanvasParticles from "../components/CanvasParticles";
 export default function Home() {
   return (
     <div className="relative min-h-screen overflow-hidden text-gray-100">
-      <StarfieldBackground />
+      <StarfieldBackground/>
       
       <main className="relative z-10 px-6 md:px-16 lg:px-32 transition-all">
         <CanvasParticles />
